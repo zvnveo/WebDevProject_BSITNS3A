8de4566a4b85acfe347d42aa30793e83f18f5fcc
zvnveo <cherish.cuizon@tup.edu.ph>
2024-01-24 22:58:55 +0800

    revised petlist.js

---
+++ src/helpers/PetList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/kitty1.jpg";
+import Margherita from "../assets/kitty2.jpg";
+import PedroTechSpecial from "../assets/kitty3.jpg";
+import Vegan from "../assets/kitty5.jpg";
+import Pineapple from "../assets/kitty6.jpg";
+import Expensive from "../assets/kitty7.jpg";
+
+export const PetList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    button: "adopt",
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    button: "adopt",
+  },
+  {
+    name: "PedroTech Special Pizza",
+    image: PedroTechSpecial,
+    button: "adopt",
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    button: "adopt",
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    button: "adopt",
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    button: "adopt",
+  },
+];
