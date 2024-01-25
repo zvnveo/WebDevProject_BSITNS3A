e1d71c8e8724a7d08e63822745b8fffb71487842
Abe Mae <abemae.saavedra@tup.edu.ph>
2024-01-25 10:32:11 +0800

    Update PetList.js
    tinagalog na cat

--- src/Helpers/PetList.js
+++ src/Helpers/PetList.js
@@ -1,39 +1,66 @@
-import Gomi from "../assets/gomi.jpg";
-import Dubu from "../assets/dubu.jpg";
-import Jinju from "../assets/jinju.jpg";
-import Kimba from "../assets/kimba.jpg";
-import Bongji from "../assets/bongji.jpg";
-import Somi from "../assets/somi.jpg";
+import Pusa1 from "../assets/kitty1.jpg";
+import Pusa2 from "../assets/kitty2.jpg";
+import Pusa3 from "../assets/kitty3.jpg";
+import Pusa4 from "../assets/kitty4.jpg";
+import Pusa5 from "../assets/kitty5.jpg";
+import Pusa6 from "../assets/kitty6.jpg";
+import Pusa7 from "../assets/kitty7.jpg";
+import Pusa8 from "../assets/kitty8.jpg";
+import Pusa9 from "../assets/kitty9.jpg";
 
 export const PetList = [
   {
-    name: "Gomi",
-    image: Gomi,
+    name: "Kitty",
+    image: Pusa1,
+    description: "WAAAAAAAAAH",
     button: "adopt",
   },
   {
-    name: "Dubu",
-    image: Dubu,
+    name: "KITTY",
+    image: Pusa2,
+    description: "IHHHHHHH",
     button: "adopt",
   },
   {
-    name: "Jinju",
-    image: Jinju,
+    name: "KITTY",
+    image: Pusa6,
+    description: "CUTIE KITTY",
     button: "adopt",
   },
   {
-    name: "Kimba",
-    image: Kimba,
+    name: "KITTY",
+    image: Pusa4,
+    description: "CUTIE KITTY",
     button: "adopt",
   },
   {
-    name: "Bongji",
-    image: Bongji,
+    name: "Pineapple Kitty",
+    image: Pusa5,
+    description: "jehlahef",
     button: "adopt",
   },
   {
-    name: "Somi",
-    image: Somi,
+    name: "KITTY",
+    image: Pusa3,
+    description: "CUTIE KITTY",
+    button: "adopt",
+  },
+  {
+    name: "Kitty",
+    image: Pusa7,
+    description: "WAAAAAAAAAH",
+    button: "adopt",
+  },
+  {
+    name: "Kitty",
+    image: Pusa8,
+    description: "WAAAAAAAAAH",
+    button: "adopt",
+  },
+  {
+    name: "Kitty",
+    image: Pusa9,
+    description: "WAAAAAAAAAH",
     button: "adopt",
   },
 ];
