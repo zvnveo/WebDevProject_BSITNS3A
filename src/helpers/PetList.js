f503dde721b615fc24536f5fe9916582bab835c8
zvnveo <cherish.cuizon@tup.edu.ph>
2024-01-25 14:05:41 +0800

    final petlist.css

--- src/helpers/PetList.js
+++ src/helpers/PetList.js
@@ -1,39 +1,66 @@
-import Pepperoni from "../assets/kitty1.jpg";
-import Margherita from "../assets/kitty2.jpg";
-import PedroTechSpecial from "../assets/kitty3.jpg";
-import Vegan from "../assets/kitty5.jpg";
-import Pineapple from "../assets/kitty6.jpg";
-import Expensive from "../assets/kitty7.jpg";
+import Pusa1 from "../assets/kitty1.jpg";
+import Pusa2 from "../assets/kitty2.jpg";
+import Pusa3 from "../assets/kitty3.jpg";
+import Pusa4 from "../assets/kitty4.jpg";
+import Pusa5 from "../assets/kitty5.jpg";
+import Pusa6 from "../assets/kitty6.jpg";
+import Pusa7 from "../assets/kitty7.jpg";
+import Pusa8 from "../assets/kitty8.jpg";
+import Pusa9 from "../assets/kitty9.jpg";
 
 export const PetList = [
   {
-    name: "Pepperoni Pizza",
-    image: Pepperoni,
+    name: "Aice",
+    image: Pusa1,
+    description: "7 months old ︱ Male",
     button: "adopt",
   },
   {
-    name: "Margherita Pizza",
-    image: Margherita,
+    name: "Caramel",
+    image: Pusa2,
+    description: "1 year old ︱ Male",
     button: "adopt",
   },
   {
-    name: "PedroTech Special Pizza",
-    image: PedroTechSpecial,
+    name: "Benchot",
+    image: Pusa6,
+    description: "1 year old ︱ Male",
     button: "adopt",
   },
   {
-    name: "Vegan Pizza",
-    image: Vegan,
+    name: "Puto",
+    image: Pusa4,
+    description: "1 year old ︱ Female",
     button: "adopt",
   },
   {
-    name: "Pineapple Pizza",
-    image: Pineapple,
+    name: "Oreo",
+    image: Pusa5,
+    description: "10 months old ︱ Male",
     button: "adopt",
   },
   {
-    name: "Very Expensive Pizza",
-    image: Expensive,
+    name: "Longi",
+    image: Pusa3,
+    description: "2 years old ︱ Female",
+    button: "adopt",
+  },
+  {
+    name: "Siopao",
+    image: Pusa7,
+    description: "1 year old ︱ Male",
+    button: "adopt",
+  },
+  {
+    name: "Chunky",
+    image: Pusa8,
+    description: "3 years old ︱ Male",
+    button: "adopt",
+  },
+  {
+    name: "Joji",
+    image: Pusa9,
+    description: "1 year old ︱ Male",
     button: "adopt",
   },
 ];
