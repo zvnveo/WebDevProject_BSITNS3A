329cd7d7fe5813ac2a0851fc0324a741215b2715
KrizcelDyn <>
2024-01-25 14:31:49 +0800

    Create PetList.js

---
+++ src/helpers/PetList.js
@@ -0,0 +1,66 @@
+import Pusa1 from "../assets/kitty1.jpg";
+import Pusa2 from "../assets/kitty2.jpg";
+import Pusa3 from "../assets/kitty3.jpg";
+import Pusa4 from "../assets/kitty4.jpg";
+import Pusa5 from "../assets/kitty5.jpg";
+import Pusa6 from "../assets/kitty6.jpg";
+import Pusa7 from "../assets/kitty7.jpg";
+import Pusa8 from "../assets/kitty8.jpg";
+import Pusa9 from "../assets/kitty9.jpg";
+
+export const PetList = [
+  {
+    name: "Aice",
+    image: Pusa1,
+    description: "7 months old ︱ Male",
+    button: "adopt",
+  },
+  {
+    name: "Caramel",
+    image: Pusa2,
+    description: "1 year old ︱ Male",
+    button: "adopt",
+  },
+  {
+    name: "Benchot",
+    image: Pusa6,
+    description: "1 year old ︱ Male",
+    button: "adopt",
+  },
+  {
+    name: "Puto",
+    image: Pusa4,
+    description: "1 year old ︱ Female",
+    button: "adopt",
+  },
+  {
+    name: "Oreo",
+    image: Pusa5,
+    description: "10 months old ︱ Male",
+    button: "adopt",
+  },
+  {
+    name: "Longi",
+    image: Pusa3,
+    description: "2 years old ︱ Female",
+    button: "adopt",
+  },
+  {
+    name: "Siopao",
+    image: Pusa7,
+    description: "1 year old ︱ Male",
+    button: "adopt",
+  },
+  {
+    name: "Chunky",
+    image: Pusa8,
+    description: "3 years old ︱ Male",
+    button: "adopt",
+  },
+  {
+    name: "Joji",
+    image: Pusa9,
+    description: "1 year old ︱ Male",
+    button: "adopt",
+  },
+];
