e42840faf2b1a4aed5f3fa106c0a26665a8db49e
Abe Mae <abemae.saavedra@tup.edu.ph>
2024-01-25 13:33:04 +0800

    create

---
+++ src/helpers/PetList.js
@@ -0,0 +1,66 @@
+import Pusa1 from "../assets/kitty1.jpg";
+import Pusa2 from "../assets/kitty2.jpg";
+import Pusa3 from "../assets/kitty3.jpg";
+import Pusa4 from "../assets/kitty4.jpg";
+import Pusa5 from "../assets/kitty5.jpg";
+import Pusa6 from "../assets/kitty6.jpg";
+import Pusa7 from "../assets/kitty7.jpg";
+import Pusa8 from "../assets/kitty8.jpg";
+import Pusa9 from "../assets/kitty9.jpg";
+
+export const PetList = [
+  {
+    name: "Kitty",
+    image: Pusa1,
+    description: "WAAAAAAAAAH",
+    button: "adopt",
+  },
+  {
+    name: "KITTY",
+    image: Pusa2,
+    description: "IHHHHHHH",
+    button: "adopt",
+  },
+  {
+    name: "KITTY",
+    image: Pusa6,
+    description: "CUTIE KITTY",
+    button: "adopt",
+  },
+  {
+    name: "KITTY",
+    image: Pusa4,
+    description: "CUTIE KITTY",
+    button: "adopt",
+  },
+  {
+    name: "Pineapple Kitty",
+    image: Pusa5,
+    description: "jehlahef",
+    button: "adopt",
+  },
+  {
+    name: "KITTY",
+    image: Pusa3,
+    description: "CUTIE KITTY",
+    button: "adopt",
+  },
+  {
+    name: "Kitty",
+    image: Pusa7,
+    description: "WAAAAAAAAAH",
+    button: "adopt",
+  },
+  {
+    name: "Kitty",
+    image: Pusa8,
+    description: "WAAAAAAAAAH",
+    button: "adopt",
+  },
+  {
+    name: "Kitty",
+    image: Pusa9,
+    description: "WAAAAAAAAAH",
+    button: "adopt",
+  },
+];
